27e4e8b3a89c98f2e5b114f401657206dfe1cd21
Sam Lockart <sam@samlockart.com>
2024-10-10 21:30:28 +1100

    remove service

--- nixos/sauron/configuration.nix
+++ nixos/sauron/configuration.nix
@@ -622,6 +622,8 @@
 
   services.tailscale = {
     enable = true;
+    authKeyFile = config.age.secrets.tailscale-authkey.path;
+    extraUpFlags = ["--login-server=https://hs.samlockart.com"];
   };
 
   # secrets
@@ -645,34 +647,6 @@
       };
     };
   };
-
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey $(cat ${config.age.secrets.tailscale-authkey.path}) --login-server=$(cat ${config.age.secrets.tailscale-server.path})
-    '';
-  };
-
 
   services.murmur = {
      enable = true;
