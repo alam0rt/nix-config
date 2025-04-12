26b999f6094404ef9e93801a518eb8c1f3a57668
alam0rt <sam@samlockart.com>
2025-04-12 10:48:47 +1000

    dont symlink

--- nixos/sauron/mail.nix
+++ nixos/sauron/mail.nix
@@ -8,17 +8,14 @@ in {
   age.secrets = {
     smtp-addr = {
       file = ../../secrets/smtp-addr.age;
-      path = "/etc/smtp.addr";
       group = "mail";
     };
     smtp-user = {
       file = ../../secrets/smtp-user.age;
-      path = "/etc/smtp.user";
       group = "mail";
     };
     smtp-pass = {
       file = ../../secrets/smtp-pass.age;
-      path = "/etc/smtp.pass";
       group = "mail";
     };
   };
