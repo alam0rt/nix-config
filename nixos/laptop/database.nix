dd0505c5fdf6f703d2e04481ad7c3ce0d5fc16c6
alam0rt <sam@samlockart.com>
2025-04-22 19:51:46 +1000

    container policy

---
+++ nixos/laptop/database.nix
@@ -0,0 +1,14 @@
+{ config
+, lib
+, pkgs
+, ... }:
+let cfg = {};
+in {
+  # ...
+  config.services.patroni = {
+    enable = true;
+    name = "foo-1";
+    scope = "foo";
+    otherNodeIps = [ "100.64.0.24" ];
+  };
+}
