2e7d70090723d310d9605ee4755c8d7fd94956e6
alam0rt <sam@samlockart.com>
2025-04-24 10:30:03 +1000

    Nix GC defaults

--- nixos/config/common/default.nix
+++ nixos/config/common/default.nix
@@ -14,4 +14,20 @@
   services.smartd = {
     enable = true;
   };
+
+  nix = {
+    # Optimisation of the Nix Store
+    optimise.automatic = true;
+    optimise.dates = [ "weekly" ];
+
+    # Garbage colection (Removes Old Snapshots)
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    channel.enable = false;
+
+  };
 }
