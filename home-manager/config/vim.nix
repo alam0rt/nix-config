5961dca74a93eee3eb5cb75ac88fc8bd10a00b7c
alam0rt <sam@samlockart.com>
2025-04-25 16:00:38 +1000

    more vim plugin

--- home-manager/config/vim.nix
+++ home-manager/config/vim.nix
@@ -10,6 +10,12 @@
       set softtabstop =4
       set shiftwidth  =4
       set expandtab
+
+      nnoremap <leader>n :NERDTreeFocus<CR>
+      nnoremap <C-n> :NERDTree<CR>
+      nnoremap <C-t> :NERDTreeToggle<CR>
+      nnoremap <C-f> :NERDTreeFind<CR>
+      autocmd VimEnter * NERDTree
     '';
     plugins = with pkgs.vimPlugins; [
         leap-nvim
@@ -17,6 +23,9 @@
         vim-go
         vim-ruby
         vim-startify
+        nerdtree
+        nerdtree-git-plugin
+        vim-devicons
     ];
   };
 }
