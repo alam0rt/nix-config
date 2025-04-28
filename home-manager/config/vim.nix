a22fd712abcdeac3ed4ea06e9b9d0893473346ff
alam0rt <sam@samlockart.com>
2025-04-28 10:57:58 +1000

    more pluginz

--- home-manager/config/vim.nix
+++ home-manager/config/vim.nix
@@ -1,21 +1,33 @@
 { pkgs, ... }: {
+  home.packages = with pkgs; [
+    nerdfonts # for devicons
+    nixd # nix-lsp
+  ];
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
     extraConfig = ''
+      " Basic settings
       set number
-
       set tabstop     =4
       set softtabstop =4
       set shiftwidth  =4
       set expandtab
 
+      " Set this variable to 1 to fix files when you save them.
+      let g:ale_fix_on_save = 1
+
+      " NERDTree
       nnoremap <leader>n :NERDTreeFocus<CR>
       nnoremap <C-n> :NERDTree<CR>
       nnoremap <C-t> :NERDTreeToggle<CR>
       nnoremap <C-f> :NERDTreeFind<CR>
-      autocmd VimEnter * NERDTree
+      autocmd VimEnter * NERDTree | wincmd p
+
+      " SOPS
+      nnoremap <leader>ef :SopsEncrypt<CR>
+      nnoremap <leader>df :SopsDecrypt<CR>
     '';
     plugins = with pkgs.vimPlugins; [
         leap-nvim
@@ -26,6 +38,14 @@
         nerdtree
         nerdtree-git-plugin
         vim-devicons
+        nvim-sops
+        fzfWrapper
+        which-key-nvim
+        vim-better-whitespace
+        vim-fugitive
+        vim-surround
+        ale
+        vim-indent-guides
     ];
   };
 }
