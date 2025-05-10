4b9b2208580ee635a552468b092f5042b0c1cbb2
alam0rt <sam@samlockart.com>
2025-05-10 17:55:04 +1000

    feede it in

--- nixos/sauron/matrix.nix
+++ nixos/sauron/matrix.nix
@@ -55,8 +55,21 @@ in {
     };
   };
 
+  age = {
+    secrets = {
+      registration-secret = {
+        file = ../../secrets/matrix-registration-secret.age;
+        owner = "matrix-synapse";
+        group = "matrix-synapse";
+      };
+    };
+  };
+
   services.matrix-synapse = {
     enable = true;
+    extraConfigFiles = [
+      config.age.registration-secret.path
+    ];
     settings.server_name = config.networking.domain;
     # The public base URL value must match the `base_url` value set in `clientConfig` above.
     # The default value here is based on `server_name`, so if your `server_name` is different
