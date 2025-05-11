9df202ef8fba32765395f91c03861befc3bd8710
alam0rt <sam@samlockart.com>
2025-05-11 10:23:27 +1000

    version

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,6 +13,7 @@
         owner = "alam0rt";
         rev = "a1e9994f5cc325a647a9df9984eec623a4b57b01";
         sha256 = "sha256-CSXmAMSVdv2G1VquHmL/28gfTWoQOpuWvaOqOmJgonk=";
+        version = "7.2.3";
       };
     });
     # example = prev.example.overrideAttrs (oldAttrs: rec {
