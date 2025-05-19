35fa9b86e5c66a8f8d1ecd16fca4940efb2e8b3d
sam <sam@samlockart.com>
2025-05-19 14:53:41 +1000

    re-enable zsh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,6 +84,8 @@
   # allow interacting with secret store
   environment.systemPackages = [inputs.agenix.packages.x86_64-linux.default];
 
+  programs.zsh.enable = true;
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
