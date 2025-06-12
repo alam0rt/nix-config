c98868e106f90d16e152dced69a183e6ce79a413
alam0rt <sam@samlockart.com>
2025-06-12 16:56:13 +1000

    fix umask

--- nixos/sauron/transmission.nix
+++ nixos/sauron/transmission.nix
@@ -29,6 +29,7 @@ in {
       trash-original-torrent-files = true;
       rpc-bind-address = "0.0.0.0";
       rpc-port = 9091;
+      umask = (builtins.fromTOML "octal = 0o0002").octal # 0666 - 0002 = u+rw,g+rw,o+r
       rpc-whitelist = "127.0.0.1,192.168.0.*,100.64.0.*";
       rpc-host-whitelist-enabled = false;
       rpc-authentication-required = false;
