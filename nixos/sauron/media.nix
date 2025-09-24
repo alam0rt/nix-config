755b14d204d78d7dce9ae572ad0751d9d62c1bd6
alam0rt <sam@samlockart.com>
2025-09-24 10:22:47 +1000

    change service name of podman

--- nixos/sauron/media.nix
+++ nixos/sauron/media.nix
@@ -125,20 +125,21 @@ in {
   ]; # dlna
   networking.firewall.allowedTCPPorts = [8191]; # flaresolverr
 
-  # selfhosted rarbg
-  # https://github.com/mgdigital/rarbg-selfhosted
   virtualisation.oci-containers.containers = {
     rarbg = {
+      # https://github.com/mgdigital/rarbg-selfhosted
       image = "ghcr.io/mgdigital/rarbg-selfhosted:latest";
       ports = ["3333:3333"];
       volumes = ["/srv/data/rarbg_db.sqlite:/rarbg_db.sqlite"];
       pull = "always";
+      serviceName = "rarbg-selfhosted";
     };
 
     flaresolverr = {
       image = "ghcr.io/flaresolverr/flaresolverr:latest";
       ports = ["8191:8191"];
       pull = "always";
+      serviceName = "flaresolverr";
     };
   };
 }
