3ce5401df4e8f2fabc845e85b61a47c550c47218
alam0rt <sam@samlockart.com>
2025-11-29 08:25:44 +1100

    use fork

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,13 +7,13 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    #    botamusique = prev.botamusique.overrideAttrs (old: rec {
-    #      src = prev.fetchFromGitHub {
-    #        repo = "botamusique";
-    #        owner = "alam0rt";
-    #        rev = "aa0b8f65847d0ac37e2bf5e11f07213751ebfdb0";
-    #        sha256 = "sha256-CSXmAMSVdv2G1VquHmL/28gfTWoQOpuWvaOqOmJgonk=";
-    #      };
+        botamusique = prev.botamusique.overrideAttrs (old: rec {
+          src = prev.fetchFromGitHub {
+            repo = "botamusique";
+            owner = "algielen";
+            rev = "190b8e3659ecbae787b0b90a3c3bbf1a4fca494a";
+            sha256 = "sha256-CSXmAMSVdv2G1VquHmL/28gfTWoQOpuWvaOqOmJgonk=";
+          };
     #      npmDeps = prev.fetchNpmDeps {
     #        src = "${src}/web";
     #        hash = "sha256-Pq+2L28Zj5/5RzbgQ0AyzlnZIuRZz2/XBYuSU+LGh3I=";
@@ -23,7 +23,7 @@
     #    });
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
-    # });
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
