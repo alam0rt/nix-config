79299b7a2c8cf70c3cb1cb9ebf679684651ed55c
alam0rt <sam@samlockart.com>
2025-11-29 10:08:45 +1100

    protobuf

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,4 +6,5 @@ pkgs: {
   rolecule = pkgs.callPackage ./rolecule {};
   scaffold = pkgs.callPackage ./scaffold {};
   opuslib-next = pkgs.callPackage ./opuslib-next {};
+  protobuf-3 = pkgs.callPackage ./protobuf-3-20-1 {};
 }
--- pkgs/protobuf-3-20-3/default.nix
+++ pkgs/protobuf-3-20-3/default.nix
@@ -1,14 +1,14 @@
 { pkgs, lib, python3Packages ? pkgs.python3Packages }:
 python3Packages.buildPythonPackage rec {
-  pname = "protobuf";
-  version = "3.20.3";
+  pname = "opuslib_next";
+  version = "1.1.5";
   src = python3Packages.fetchPypi {
     inherit pname version;
   };
 
   meta = with lib; {
-    description = "Protocol Buffers are Google’s data interchange format";
-    homepage = "https://pypi.org/project/protobuf/";
+    description = "Python bindings for Opus codec (next generation)";
+    homepage = "https://pypi.org/project/opuslib_next/";
     license = licenses.bsd3;
     maintainers = with maintainers; [ ];
   };
