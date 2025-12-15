19cb758585c1599d2b4c6e7cb6977240eb0cda28
alam0rt <sam@samlockart.com>
2025-12-15 13:27:09 +1100

    try this

--- nixos/config/common/agenix.nix
+++ nixos/config/common/agenix.nix
@@ -2,9 +2,9 @@
   age = {
     rekey = {
       masterIdentities = [
-        ../../secrets/yubikey-22916238.pub
-        ../../secrets/yubikey-15498888.pub
-        ../../secrets/yubikey-18103415.pub
+        ./pubkeys/yubikey-22916238.pub
+        ./pubkeys/yubikey-15498888.pub
+        ./pubkeys/yubikey-18103415.pub
       ];
       storageMode = "local";
       localStorageDir = ./. + "/secrets/rekeyed/${config.networking.hostName}";
