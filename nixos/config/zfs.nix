69cf439b9e1938315857553a6888b565e7b81f33
alam0rt <sam@samlockart.com>
2025-12-29 12:02:58 +1100

    add auto prune

--- nixos/config/zfs.nix
+++ nixos/config/zfs.nix
@@ -9,6 +9,10 @@
     zfs.package = pkgs.zfs_unstable;
   };
 
+  environment.systemPackages = with pkgs; [
+    zfs-prune-snapshots
+  ];
+
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
   ''; # zfs already has its own scheduler. without this my(@Artturin) computer froze for a second when i nix build something.
