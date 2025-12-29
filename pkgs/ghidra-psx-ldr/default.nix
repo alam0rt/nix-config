8f85367eb9e367bd22ddb762486216056701181f
alam0rt <sam@samlockart.com>
2025-12-30 08:59:29 +1100

    compile sla

--- pkgs/ghidra-psx-ldr/default.nix
+++ pkgs/ghidra-psx-ldr/default.nix
@@ -15,6 +15,14 @@ ghidra.buildGhidraExtension (finalAttrs: {
     fetchSubmodules = true;
   };
 
+  # Compile the SLEIGH file before building the extension
+  preBuild = ''
+    # Remove the pre-compiled .sla file (it's in an old XML format)
+    rm -f data/languages/mips32le.sla
+    # Compile the .slaspec file using Ghidra's sleigh compiler
+    ${ghidra}/lib/ghidra/support/sleigh data/languages/mips32le.slaspec data/languages/mips32le.sla
+  '';
+
   meta = {
     description = "Sony PlayStation PSX executables loader for Ghidra";
     homepage = "https://github.com/lab313ru/ghidra_psx_ldr";
